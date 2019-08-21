722a6699b3ed31fcbd6542157f6ca3ea1faeff5f
yaojiaqi <jiaqi.yao@brilliantaero.com>
2019-08-21 16:49:05 +0800

    回退老版本

--- src/js/tools.js
+++ src/js/tools.js
@@ -1,6 +1,6 @@
 function myTools() {
-    const bndongTools = this;
-    const colors      = {
+    var bndongTools = this;
+    var colors      = {
         "gray": "color: #1B2B34;font-size: 12px; line-height: 18px;",
         "red": "color: #EC5f67;font-size: 12px; line-height: 18px;",
         "orange": "color: #F99157;font-size: 12px; line-height: 18px;",
@@ -32,27 +32,26 @@ function myTools() {
      * 控制台输出内容
      */
     this.consoleText = function(list, mode) {
-        rHref = 'https://github.com/'+window.cnblogsConfig.GhUserName+'/'+window.cnblogsConfig.GhRepositories+'/tree/'+window.cnblogsConfig.GhVersions;
-        var e = ["\n %c %c %c Theme GitHub - "+(window.cnblogsConfig.GhVersions).substring(0,7)+" %c  %c "+rHref+"  %c \n\n", "background: #fadfa3; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "background: #FCEDC9; padding:5px 0;", "background: #fadfa3; padding:5px 0;"];
-        // window.console.log.apply(console, e);
+        var e = ["\n %c %c %c Theme GitHub %c  %c https://github.com/BNDong/Cnblogs-Theme-SimpleMemory  %c \n\n", "background: #fadfa3; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "background: #FCEDC9; padding:5px 0;", "background: #fadfa3; padding:5px 0;"];
+        window.console.log.apply(console, e);
         switch (mode) {
             case 'random':
                 var colorList = [colors.red, colors.orange, colors.yellow, colors.green, colors.teal, colors.blue, colors.purple, colors.brown];
                 $.each(list, function (i) {
                     var str = (list[i]).toString();
                     var ind = bndongTools.randomNum(0, colorList.length - 1);
-                    // console.log('%c'+str, colorList[ind]);
+                    console.log('%c'+str, colorList[ind]);
                 });
                 break;
             case 'banner':
                 $.each(list, function (i) {
                     var fl = list[i];
-                    // console.log('\n' + ' %c '+(fl[0])+' %c '+(fl[1])+' ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+                    console.log('\n' + ' %c '+(fl[0])+' %c '+(fl[1])+' ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
                 });
                 break;
 
             default:
-                // console.log('%c'+list.join('\n'), colors.gray);
+                console.log('%c'+list.join('\n'), colors.gray);
                 break;
         }
     };
@@ -61,7 +60,7 @@ function myTools() {
      * 滚动主体滚动条到指定位置
      */
     this.actScroll = function(endScroll, time) {
-        $('html,body').stop().animate({scrollTop: endScroll}, time);
+        $('html,body').animate({scrollTop: endScroll}, time);
     };
 
     /**

c89213e3db300528126b0f522422613db8c62c4f
yaojiaqi <jiaqi.yao@brilliantaero.com>
2019-08-21 18:39:14 +0800

    font_bug

--- js/config.js
+++ js/config.js
@@ -83,11 +83,11 @@ require.config({
         MyTween: {
             deps: ['TweenMax']
         },
+        // 'css!https://cdn.webfont.youziku.com/webfonts/nomal/111379/47284/5a531cbbf629dc07a8fbd011.css', // 有字库字体
         base: {
             deps: [
                 'tools',
                 'css!https://at.alicdn.com/t/font_543384_0zv65huqu00i.css', // 阿里云字体图标
-                'css!https://cdn.webfont.youziku.com/webfonts/nomal/111379/47284/5a531cbbf629dc07a8fbd011.css', // 有字库字体
             ]
         }
     }
